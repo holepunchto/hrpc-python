31cc1eacb00ed9991f201debe37b82b47c52107d
Balakrishna Avulapati <ba@bavulapati.com>
2026-08-06 18:10:53 +0530

    run hrpc-test's dispatch vectors

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
 require('./test/skeleton.test.js') // generator unit + golden tests
 require('./test/e2e.test.js') // end-to-end scenarios against a real Python interpreter
+require('./test/vectors.test.js') // dispatch conformance against hrpc-test's vectors
